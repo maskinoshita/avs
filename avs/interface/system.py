1d7b5d20c7570b7796d703ea299e8080ebf1e16f
maskinoshita <maskinoshita@users.noreply.github.com>
2018-06-04 06:11:58 +0900

    Update system.py

--- avs/interface/system.py
+++ avs/interface/system.py
@@ -31,7 +31,7 @@ class System(object):
                 "settings": [
                     {
                         "key": "locale",
-                        "value: "ja-JP"
+                        "value": "ja-JP"
                     }
                 ]
             }
